d2e0a7901ccb5500dcf1376800a5c3dcf3eb4c42
rafael-vasconcellos <rafael.vasconcelos7497@gmail.com>
2026-04-16 04:56:28 -0300

    Create Dockerfile

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -61,8 +61,8 @@ async function main() {
 
 main()
   .catch((e) => {
-    //console.error(e);
-    process.exit(1);
+    console.error(e);
+    //process.exit(1);
   })
   .finally(async () => {
     await prisma.transaction.deleteMany({})
