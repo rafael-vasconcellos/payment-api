50fb488bc55bf239806f87754abb729d7f3467da
rafael-vasconcellos <rafael.vasconcelos7497@gmail.com>
2026-04-16 00:29:27 -0300

    Update transaction authorization logic to use the actual mock REST

--- src/infra/providers/Auth/Transaction/index.ts
+++ src/infra/providers/Auth/Transaction/index.ts
@@ -2,10 +2,26 @@ import { ITransactionAuthProvider } from "src/core/providers/Auth/ITransactionAu
 import { ICreateTransactionDTO } from "src/core/useCases/Transaction/ICreateTransaction";
 
 
+
+interface IProviderResponse {
+    status: "success" | "fail"
+    data: {
+        authorization: boolean
+    }
+}
+
 export class ProtoTransactionAuthProvider implements ITransactionAuthProvider {
     async auth(createTransactionDTO: ICreateTransactionDTO) {
-        const n = Math.round(Math.random()*10)
-        if (n > 1.5) { return true }
-        else { return false }
+        const response = await fetch("https://util.devi.tools/api/v2/authorize")
+        if (!response.ok) return await this.authMock()
+        const response_data: IProviderResponse = await response.json()
+        return response_data.data.authorization
     }
+
+    async authMock(): Promise<boolean> {
+        const n = Math.round(Math.random()*10)
+        if (n > 1.5) return true
+        return false
+    }
+
 }
